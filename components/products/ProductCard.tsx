c4aac7d6b587586a801a25f042e7bed267cf0c30
Melina Sciarratta <melisciar@gmail.com>
2023-09-28 15:07:46 -0300

    mostrar una nueva imagen al hacer hover

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,12 @@
-import { Grid, Card, CardActionArea, CardMedia } from "@mui/material";
+import { useMemo, useState } from "react";
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Box,
+  Typography,
+} from "@mui/material";
 import { IProduct } from "@/interfaces";
 
 interface Props {
@@ -6,17 +14,36 @@ interface Props {
 }
 
 export const ProductCard: React.FC<Props> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            image={productImage}
             alt={product.title}
+            className="fadeIn"
           />
         </CardActionArea>
       </Card>
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
     </Grid>
   );
 };
