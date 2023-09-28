6b0468fb79161ba7a54f740290bb67a2ea4a89ef
Melina Sciarratta <melisciar@gmail.com>
2023-09-28 14:07:16 -0300

    página de inicio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { initialData } from "@/database/products";
 
 export default function Home() {
   return (
@@ -12,6 +19,21 @@ export default function Home() {
         <Typography variant="h2" sx={{ mb: 1 }}>
           Todos los productos
         </Typography>
+        <Grid container spacing={4}>
+          {initialData.products.map((product) => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </ShopLayout>
     </>
   );
