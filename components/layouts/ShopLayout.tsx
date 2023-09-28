ab6836548ed3e21d27fbc8ce07244858f7b3ae0f
Melina Sciarratta <melisciar@gmail.com>
2023-09-28 14:26:57 -0300

    menú lateral

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 interface Props {
   title: string;
@@ -26,7 +26,7 @@ export const ShopLayout: React.FC<Props> = ({
       <nav>
         <Navbar />
       </nav>
-      {/** TODO: Sidebar */}
+      <SideMenu />
       <main
         style={{ margin: "80px auto", maxWidth: "1440px", padding: "0px 30px" }}
       >
