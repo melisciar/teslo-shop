ba7966aff6004c148359f61905edba8ccfd85162
Melina Sciarratta <melisciar@gmail.com>
2023-09-28 14:47:37 -0300

    mostrar la grilla de inicio desde componentes

---
+++ components/products/ProductList.tsx
@@ -0,0 +1,17 @@
+import { Grid } from "@mui/material";
+import { IProduct } from "@/interfaces";
+import { ProductCard } from ".";
+
+interface Props {
+  products: IProduct[];
+}
+
+export const ProductList: React.FC<Props> = ({ products }) => {
+  return (
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <ProductCard key={product.slug} product={product} />
+      ))}
+    </Grid>
+  );
+};
