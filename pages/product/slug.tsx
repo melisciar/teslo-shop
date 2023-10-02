540d362f2e713cb8383176e18f558d02406ea5a1
Melina Sciarratta <melisciar@gmail.com>
2023-10-02 10:08:03 -0300

    Contador de productos para agregar al carrito

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import { ShopLayout } from "@/components/layouts";
 import { initialData } from "@/database/products";
 import { ProductSlideshow } from "@/components/products";
+import { ItemCounter } from "@/components/ui";
 
 const product = initialData.products[0];
 
@@ -25,7 +26,7 @@ const ProductPage = () => {
             {/** Cantidad */}
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Cantidad</Typography>
-              {/** //TODO: ItemCounter */}
+              <ItemCounter />
             </Box>
             {/** Agregar al carrito */}
             <Button color="secondary" className="circular-btn">
