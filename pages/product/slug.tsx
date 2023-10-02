f1b2474cf1067e022b3e6133d886844e85b2b212
Melina Sciarratta <melisciar@gmail.com>
2023-10-02 10:19:48 -0300

    selector de talles

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import { ShopLayout } from "@/components/layouts";
 import { initialData } from "@/database/products";
-import { ProductSlideshow } from "@/components/products";
+import { ProductSlideshow, SizeSelector } from "@/components/products";
 import { ItemCounter } from "@/components/ui";
 
 const product = initialData.products[0];
@@ -27,6 +27,7 @@ const ProductPage = () => {
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Cantidad</Typography>
               <ItemCounter />
+              <SizeSelector sizes={product.sizes} />
             </Box>
             {/** Agregar al carrito */}
             <Button color="secondary" className="circular-btn">
